10fcf7d5a4512dc33ea03eca0242512730914716
WojDlu <wojdluzewski@gmail.com>
2020-09-23 13:20:00 +0200

    first commit

---
+++ src/test/java/com/wojdlu/demo/CovidAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wojdlu.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
